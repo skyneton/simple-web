f1174813d54cfce8aee31c7363461daf6fde9192
skyneton <67633420+skyneton@users.noreply.github.com>
2023-08-01 21:02:48 +0900

    Update index.php

--- index.php
+++ index.php
@@ -36,9 +36,8 @@ if(isset($_GET['query'])) {
         <td>TITLE</td>
     </tr>
     <?php 
-    echo $cursor;
-    echo isset($cursor);
-    echo $cursor->fetch_assoc();
+    echo "CURSOR".gettype($cursor);
+    echo "ISSET".isset($cursor);
     echo trim(NULL);
     while($row = $cursor->fetch_assoc()) {
         echo $row["id"];
