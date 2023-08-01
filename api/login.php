d736dcad62deebfa2490d19b1eabbe4d296e572c
skyneton <67633420+skyneton@users.noreply.github.com>
2023-08-01 21:49:47 +0900

    Update login.php

--- api/login.php
+++ api/login.php
@@ -7,12 +7,14 @@ if(isset($_SESSION["uid"])) {
 require_once("../db.php");
 require_once("../utils.php");
 
-$id = trim_or_empty($_POST["id"]);
-$pw = trim_or_empty($_POST["pw"]);
-if(strlen($id) <= 0 || strlen($pw) <= 0) {
-    http_response_code(400);
-    exit;
-}
+// $id = trim_or_empty($_POST["id"]);
+// $pw = trim_or_empty($_POST["pw"]);
+// if(strlen($id) <= 0 || strlen($pw) <= 0) {
+//     http_response_code(400);
+//     exit;
+// }
+$id = "test"
+$pw = "test"
 
 $mysqli = db_connect();
 create_table($mysqli, "user", "id INTEGER AUTO_INCREMENT PRIMARY KEY, uid TEXT, pw TEXT");
@@ -23,6 +25,7 @@ $stmt->prepare("SELECT * FROM user WHERE uid = ? AND pw = ?;");
 $stmt->bind_param($id, $pw);
 $stmt->execute();
 $cursor = $stmt->get_result();
+echo mysql_num_rows($cursor);
 if(mysql_num_rows($cursor) >= 1) {
     $row = $cursor->fetch_assoc();
     $_SESSION["uid"] = $row["id"];
