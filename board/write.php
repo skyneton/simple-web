ef7c2f53b438a39e5523fa58c2e7aac9656db3e9
skyneton <67633420+skyneton@users.noreply.github.com>
2023-08-01 22:13:06 +0900

    Update write.php

--- board/write.php
+++ board/write.php
@@ -3,7 +3,9 @@ require_once("../db.php");
 require_once("../utils.php");
 session_start();
 
-$board_id = trim_or_empty($_GET['id']);
+$board_id = "";
+if(isset($_GET['id']))
+    $board_id = trim_or_empty($_GET['id']);
 
 $mysqli = db_connect();
 create_table($mysqli, "board", "id INTEGER PRIMARY KEY AUTO_INCREMENT, writter TEXT, title TEXT, body TEXT");
